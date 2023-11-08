5b45401b509c004eedcea9bd576595f9f2192a42
Emily Zeng <zhixuan.zeng@gmail.com>
2023-11-08 13:17:15 -0500

    made subtree script more friendly

--- medium-git-subtree.sh
+++ medium-git-subtree.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 # from https://gist.github.com/icheko/9ff2a0a90ef2b676a5fc8d76f69db1d3
-SOURCE_BRANCH="bgkeeb-source"
-STAGING_BRANCH="bgkeeb-staging"
+# before running this script, run 
+shield_name="bgkeeb"
+remote_repo="ezxzeng/zmk-bgkeeb"
+remote_branch="main"
+
+git remote -v | grep -w ${shield_name} || git remote add ${shield_name} git@github.com:${remote_repo}.git
+SOURCE_BRANCH="${shield_name}-source"
+STAGING_BRANCH="${shield_name}-staging"
 
 echo
-echo "Fetch new changes from bgkeeb"
+echo "Fetch new changes from ${shield_name}"
 echo -----------------------------------------
-git fetch bgkeeb main
+git fetch ${shield_name} ${remote_branch}
 
-BGKEEB_LATEST_COMMIT=`git ls-remote bgkeeb | grep "refs/heads/main" | awk '{ print $1}'`
+LATEST_COMMIT=`git ls-remote ${shield_name} | grep "refs/heads/${remote_branch}" | awk '{ print $1}'`
 echo
-echo "bgkeeb latest commit: ${HELMCHARTS_LATEST_COMMIT}"
+echo "${shield_name} latest commit: ${HELMCHARTS_LATEST_COMMIT}"
 echo
 
 # checkout source repo
-git checkout -b ${SOURCE_BRANCH} bgkeeb/main
-# echo Hi. >> config/boards/shields/bgkeeb/test.txt
-# git add config/boards/shields/bgkeeb/test.txt
+git checkout -b ${SOURCE_BRANCH} ${shield_name}/${remote_branch}
+# echo Hi. >> config/boards/shields/${shield_name}/test.txt
+# git add config/boards/shields/${shield_name}/test.txt
 # git commit -am "test"
 
 # create new staging branch from all the commits impacting "/my-chart" from source repo
-git subtree split -P config/boards/shields/bgkeeb -b ${STAGING_BRANCH} --onto main
+git subtree split -P config/boards/shields/${shield_name} -b ${STAGING_BRANCH} --onto main
 
 # checkout master
 git checkout main
